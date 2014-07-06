15a341b027366a5cf3f6f3b8c0506b248045ed90
Rogerio Medeiros <argerim@gmail.com>
2014-07-06 05:06:12 -0300

    helloworld 2.0

--- gen/com/rogeriomedeiros/helloandroid/R.java
+++ gen/com/rogeriomedeiros/helloandroid/R.java
@@ -14,14 +14,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int textView1=0x7f060000;
+        public static final int nomeEditText=0x7f060000;
+        public static final int saudacaoButton=0x7f060001;
+        public static final int saudacaoTextView=0x7f060002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
+        public static final int action_settings=0x7f040006;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int saudacao=0x7f040004;
+        public static final int surpreenda_me=0x7f040002;
+        public static final int title_activity_main=0x7f040005;
+        public static final int titulo=0x7f040003;
     }
     public static final class style {
         /** 
